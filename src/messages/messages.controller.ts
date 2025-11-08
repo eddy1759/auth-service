804c00943f52df7e6add27486b1537ff04b5dd26
Edet Asuquo <eddy1759@gmail.com>
2025-11-08 10:42:30 +0100

    feat: commit

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,11 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { GetUser } from './get-user.decorator';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(private readonly prisma: PrismaService) {}
+
   @Get(':id')
-  getMessageThread(@Param('id') messageId: string) {
+  getMessageThread(@Param('id') messageId: string, @GetUser() user: any) {
     return this.prisma.message.findUnique({
       where: { id: messageId },
+      include: { sender: true, receiver: true },
     });
   }
 }
